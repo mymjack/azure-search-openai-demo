4d59cea09698472ff4c0edc1525ef9563c19c310
Karan Brar <karanbirsingh.brar@mail.utoronto.ca>
2023-06-28 10:19:10 -0400

    Modify same questions on the UI to be BMO specific

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What benefits do you offer for students?",
+        value: "What benefits do you offer for students?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "Does BMO have any first-time home buyer incentives?", value: "Does BMO have any first-time home buyer incentives?" },
+    { text: "What travel credit cards can I get?", value: "What travel credit cards can I get?" }
 ];
 
 interface Props {
