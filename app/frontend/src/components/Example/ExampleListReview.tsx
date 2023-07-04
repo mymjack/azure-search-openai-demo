825c4965b4bd752247f8b98b76b6d905281df8d9
Xinshang Yin <headyin@gmail.com>
2023-07-04 14:01:42 -0400

    update app review example

--- app/frontend/src/components/Example/ExampleListReview.tsx
+++ app/frontend/src/components/Example/ExampleListReview.tsx
@@ -11,8 +11,8 @@ const EXAMPLES: ExampleModel[] = [
     { text: "What are the top 10 topics since 2023-01-01?", value: "What are the top 10 topics since 2023-01-01?" },
     { text: "What's the new topics in version 5.43.1?", value: "What's the new topics in version 5.43.1?" },
     {
-        text: "Show me the review that has 'login error' in the topic since 2023-01-01.",
-        value: "Show me the review that has 'login error' in the topic since 2023-01-01."
+        text: "What's the only topics in May 2023?",
+        value: "What's the only topics in May 2023?"
     }
 ];
 
